7d6dc67dca7846623b850087af4f20441a1dbabf
Elmon11 <elmon11bussiness@gmail.com>
2020-11-21 13:16:55 +0100

    Update developer.py

--- cogs/commands/developer.py
+++ cogs/commands/developer.py
@@ -10,16 +10,23 @@ class Developer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
+    async def connect_to(self, guild_id: int, channel_id: str):
+        ws = self.bot._connection._get_websocket(guild_id)
+        await ws.voice_state(str(guild_id), channel_id)    
+    
     @commands.command(help='Shuts down the bot', aliases=['kill', 'restart'])
     @checks.is_dev()
     async def shutdown(self, ctx):
         await ctx.send(embed=discord.Embed(description=f'{ctx.author.name} has shut down the bot.', colour=0xbc0a1d))
         print(f'[{datetime.datetime.utcnow().replace(microsecond=0)} INFO]: [Shutdown] {ctx.author.name}: shutting down {self.bot.user.name} command sent in {ctx.guild.name}')
+        for player in self.bot.music.player_manager.find_all():
+            self.bot.music.player_manager.remove(player.guild_id)
+            await self.connect_to(player.guild_id, None)
         await self.bot.logout()
     
     @commands.command(name='eval')
     @checks.is_dev()
-    async def eval(self, ctx, *, cmd):
+    async def evaluate(self, ctx, *, cmd):
         'Executes a line of code.'
         try:
             result = eval(cmd)
@@ -78,6 +85,20 @@ class Developer(commands.Cog):
                 blacklistMessage += f'- {user.name}\n'
             blacklistEmbed = discord.Embed(description=blacklistMessage, title='Current blacklist', colour = 0xbc0a1d)
             await ctx.send(embed=blacklistEmbed)
-
+    
+    @commands.command(pass_context=True)
+    @checks.is_dev()
+    async def listnodm(self, ctx):
+        if not checks.nodm_ids:
+            await ctx.send(embed=discord.Embed(description='No users currently opted out of DMs.', colour=0xbc0a1d))
+        else:
+            print(f'[{datetime.datetime.utcnow().replace(microsecond=0)} INFO]: [Event] Showing {ctx.author.name} the No-DM List.')
+            nodmMessage = ''
+            for user in checks.nodm_ids:
+                user = self.bot.get_user(user)
+                nodmMessage += f'- {user.name}\n'
+            nodmEmbed = discord.Embed(description=nodmMessage, title='Current No-DM list', colour = 0xbc0a1d)
+            await ctx.send(embed=nodmEmbed)
+    
 def setup(bot):
     bot.add_cog(Developer(bot))
